c511f8a5854db5970df2732247a5169f2190c4e5
Rajadithya <50296370+Rajadithya@users.noreply.github.com>
2020-05-08 21:18:58 +0530

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 $(".login-btn").click(function(){
 alert('Hello World')
-$(".container").append("1")
+$("h1").append("1")
   var firebaseConfig = {
     apiKey: "AIzaSyAGt0RdAXqs9pi6-H2pMzy_SIq8CF4OQFY",
     authDomain: "trialcasesdatabase.firebaseapp.com",
