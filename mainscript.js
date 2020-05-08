7ee114245b23daa0deed9795f2bd50d31d790409
Rajadithya <50296370+Rajadithya@users.noreply.github.com>
2020-05-08 21:07:43 +0530

    Create mainscript.js

---
+++ mainscript.js
@@ -0,0 +1,20 @@
+$(".login-btn").click(function(){
+alert('Hello World')
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyAGt0RdAXqs9pi6-H2pMzy_SIq8CF4OQFY",
+    authDomain: "trialcasesdatabase.firebaseapp.com",
+    databaseURL: "https://trialcasesdatabase.firebaseio.com",
+    projectId: "trialcasesdatabase",
+    storageBucket: "trialcasesdatabase.appspot.com",
+    messagingSenderId: "516787338667",
+    appId: "1:516787338667:web:f40be2dbaf56671910a6ee"
+  };
+  // Initialize Firebase
+  //firebase.initializeApp(firebaseConfig);
+   var rootRef = firebase.database().ref("https://trialcasesdatabase.firebaseio.com/");
+
+  //rootRef.child('first').set('Ada');
+
+$("h1").hide()
+});
